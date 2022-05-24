0426a7d05892404030b8bb498afe46c1b1d67f6a
Demon Fabien <demon.fabien@ynov.com>
2022-05-24 14:47:49 +0200

    site online

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -3,7 +3,7 @@ function sendEmail (){
         Host : "smtp.gmail.com",
         Username : "demon.fabien.pro@gmail.com",
         Password : "Fabionicuss",
-        To : 'demon.fabien.pro@gmail.com',
+        To : "demon.fabien.pro@gmail.com",
         From : document.getElementById("email").value,
         Subject : document.getElementById("subject").value,
         Body : "Firstname :" + document.getElementById("firstname").value +"\t"+
